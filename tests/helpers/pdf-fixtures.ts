7c44838b7ac50a21d62314b024a924851ce9d768
Maximiliam Altrock <m.altrock@hotmail.de>
2026-02-10 14:49:12 +0100

    ...

--- tests/helpers/pdf-fixtures.ts
+++
@@ -1,9 +0,0 @@
-import { PDFDocument } from 'pdf-lib'
-
-export async function createPdfBytes(pageCount = 1): Promise<Uint8Array> {
-  const pdfDoc = await PDFDocument.create()
-  for (let i = 0; i < pageCount; i++) {
-    pdfDoc.addPage()
-  }
-  return pdfDoc.save()
-}
